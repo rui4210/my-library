bc03dd4fb8869808038ab3ae13fd8b45202915c0
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-09-30 16:43:59 +0900

    カラム設定、バリデーション設定

---
+++ db/migrate/20200930052446_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title,   null: false
+      t.string :genre
+      t.string :author
+      t.string :publishing
+      t.string :record_date,  null: false
+      t.text :memo
+      t.text :impression
+      t.integer :reading_situation_id, null: false
+      t.timestamps
+    end
+  end
+end
