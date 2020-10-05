a587c35367e7ed49ccf2efe45d477fb121898da5
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-10-05 17:12:45 +0900

    ユーザー登録ページ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :items
+
+  #ユーザー情報
+  #ネームが必須であること
+  validates :name, presence: true
+  #メールアドレスが必須であること
+  validates :email, presence: true
+  #メールアドレスが一意性であること
+  validates :email, uniqueness: true
+  #メールアドレスは@を含む必要があること
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  #パスワードが必須であること
+  validates :password, presence: true
+  #パスワードは6文字以上であること
+  validates :password, length: { minimum: 6 }
+  #パスワードは半角英数字混合であること
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 end
